219e97102b211fb7d64f88e3e29f2cf9aa5fb589
cawdly <527197388@qq.com>
2017-10-13 10:56:09 +0800

    First Commit

---
+++ Assets/EnhancedScroller v2/Demos/Main Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EnhancedScollerDemos.MainMenu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void SceneButton_OnClick(string sceneName)
+        {
+            Application.LoadLevel(sceneName);
+        }
+    }
+}
---
+++ Assets/EnhancedScroller v2/Demos/Main Menu/ReturnToMainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EnhancedScrollerDemos.MainMenu
+{
+    public class ReturnToMainMenu : MonoBehaviour
+    {
+        public void ReturnToMainMenuButton_OnClick()
+        {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
